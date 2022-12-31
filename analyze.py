48d00ea9b24ecd8bdeb9c1265159659c5521ab41
efenton32 <eliotfenton@gmail.com>
2022-12-30 19:17:43 -0500

    Improved analysis script

--- analyze.py
+++ analyze.py
@@ -7,9 +7,10 @@ save_path = "/n/home12/efenton/for_windows/gpsfs/"
 load_path = "sims/NC_044049/"
 sim_file = "spec_batch_{}_rep_{}.hdf5"
 data_file = "../2sfs/twosfs/agl_data/NC_044049/cod_NC_044049_initial_spectra.hdf5"
+num_sims = 20
 
 ks = []
-with open(load_path + "results_7.jsonl") as df:
+with open(load_path + "results_8.jsonl") as df:
     for line in df:
         try:
             ks.append(float(json.loads(line)["ks"]))
@@ -18,20 +19,18 @@ with open(load_path + "results_7.jsonl") as df:
 
 idx = np.argmin(ks)
 
-batch = idx // 100 + 1
-rep = idx % 100
+batch = idx // num_sims + 1
+rep = idx % num_sims
 
-print(idx)
-print(batch)
-print(rep)
+print("batch " + str(batch) + ", rep " + str(rep))
 
 spec_sim = load_spectra(load_path + sim_file.format(batch, rep))
 spec_data = load_spectra(data_file)
 
 plt.figure()
-plt.loglog(np.arange(126) + 1, spec_data.normalized_onesfs(folded=True)[1:], "x", label = "data")
-plt.loglog(np.arange(126) + 1, spec_sim.normalized_onesfs(folded=True)[1:], "x", label = "sim")
+plt.loglog(np.arange(126)[:63] + 1, spec_sim.normalized_onesfs(folded=True)[1:64], "x", label = "sim")
+plt.loglog(np.arange(126)[:63] + 1, spec_data.normalized_onesfs(folded=True)[1:64], "x", label = "data")
 plt.legend()
-plt.savefig(save_path + "test1.png")
+plt.savefig(save_path + "test_param_change.pdf")
 plt.close()
 
