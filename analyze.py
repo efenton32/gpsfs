b7ba23913cb7cbb70d470026334aab1918a22cd2
efenton32 <eliotfenton@gmail.com>
2022-12-30 20:00:09 -0500

    Improved analysis.py ease of use

--- analyze.py
+++ analyze.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from twosfs.spectra import Spectra, load_spectra
 import json
+import sys
 
 save_path = "/n/home12/efenton/for_windows/gpsfs/"
 load_path = "sims/NC_044049/"
@@ -9,15 +10,19 @@ sim_file = "spec_batch_{}_rep_{}.hdf5"
 data_file = "../2sfs/twosfs/agl_data/NC_044049/cod_NC_044049_initial_spectra.hdf5"
 num_sims = 20
 
-ks = []
-with open(load_path + "results_8.jsonl") as df:
-    for line in df:
-        try:
-            ks.append(float(json.loads(line)["ks"]))
-        except:
-            pass
-
-idx = np.argmin(ks)
+if sys.argv[1] == "idx":
+    idx = int(sys.argv[2])
+elif sys.argv[1] == "batch":
+    ks = []
+    with open(load_path + "results_{}.jsonl".format(sys.argv[2])) as df:
+        for line in df:
+            try:
+                ks.append(float(json.loads(line)["ks"]))
+            except:
+                pass
+    idx = np.argmin(ks)
+else:
+    raise ValueError("Must be either an idx or batch")
 
 batch = idx // num_sims + 1
 rep = idx % num_sims
